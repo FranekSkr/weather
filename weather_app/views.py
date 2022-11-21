fc1b841f6e57a8defb459770d4e1245bc6f1c497
FranekSkr <franek.skrobot@gmail.com>
2022-11-21 20:47:40 +0100

    csrf

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 import json
 import urllib.request, urllib.error
+from django.views.decorators.csrf import csrf_exempt
 
 
 
 # Create your views here.
 
-
+@csrf_exempt
 def index(request):
     def removeAccents(input_text):
 
